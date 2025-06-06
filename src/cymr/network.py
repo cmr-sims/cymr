a816f62ef4040c7ebfb887293947fcc46399d7c6
Neal Morton <mortonne@gmail.com>
2025-06-06 00:51:58 -0500

    fix: non-dynamic retention

--- src/cymr/network.py
+++ src/cymr/network.py
@@ -52,7 +52,7 @@ def prepare_study_param(n_item, n_sub, B, Lfc, Lcf, distract_B=None, retention_B
     param = {'B': B, 'Lfc': Lfc, 'Lcf': Lcf, 'distract_B': None}
     if distract_B is not None:
         distract_B = expand_param(distract_B, (n_item, n_sub))
-        if retention_B.shape[0] > 1:
+        if isinstance(retention_B, np.ndarray) and retention_B.shape[0] > 1:
             # if there is a value for each item, use the last one
             retention_B = retention_B[-1]
         retention_B = expand_param(retention_B, (1, n_sub))
